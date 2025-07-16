f4a556b9797982fc77d374ad8da961fc343bff09
Gino Filicetti <ginof@google.com>
2025-07-16 17:35:23 -0400

    fixed email variable

--- deploy/scripts/1-enable-apis.sh
+++ deploy/scripts/1-enable-apis.sh
@@ -53,7 +53,7 @@ for ROLE in "${ROLES[@]}"
 do
   echo "Granting $ROLE..."
   gcloud projects add-iam-policy-binding "$PROJECT" \
-    --member=serviceAccount:$SA_EMAIL \
+    --member=serviceAccount:$VERTEX_SA_EMAIL \
     --role="$ROLE" \
     --condition=None > /dev/null
 done
