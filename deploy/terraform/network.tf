a404b748073771f079d8414e8c58aeb82996dfb3
Gino Filicetti <ginof@google.com>
2025-07-22 16:32:14 -0400

    fixed array typo

--- deploy/terraform/network.tf
+++ deploy/terraform/network.tf
@@ -50,7 +50,7 @@ data "google_compute_subnetwork" "existing" {
 # Set up the firewall correctly
 resource "google_compute_firewall" "firewall" {
   name    = "allow-common"
-  network = google_compute_network.default.name
+  network = google_compute_network.default[0].name
 
   allow {
     protocol = "icmp"
